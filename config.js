c051cdb97c56f57fa44f9c998d5519f47fdf482d
Nicolas Dibot <NicolasDibot@users.noreply.github.com>
2026-07-02 21:05:52 +0200

    Brancher le front sur le backend Render

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Renseigner l'URL publique du backend apres deploiement, par exemple :
 // window.AUDREY_API_BASE_URL = "https://orthophonie-audrey-api.onrender.com";
-window.AUDREY_API_BASE_URL = "";
+window.AUDREY_API_BASE_URL = "https://orthophonie-audrey-api.onrender.com";
